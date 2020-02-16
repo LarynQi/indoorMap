8ea77d3b4e3c41c3efb50c960035ff9efcc0dcec
Natalia Luzuriaga <natalia.luzuriaga@Natalias-MacBook-Pro-3.local>
2020-02-15 17:38:52 -0800

    app

---
+++ app/umaps/Square.swift
@@ -0,0 +1,85 @@
+//
+//  Square.swift
+//  umaps
+//
+//  Created by Natalia Luzuriaga on 2/15/20.
+//  Copyright © 2020 treehacks. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Square {
+//    static func == (lhs: Square, rhs: Square) -> Bool {
+//            return lhs.valid == rhs.valid
+//                && lhs.plusCode == rhs.plusCode
+//                && lhs.color == rhs.color
+//                && lhs.size == rhs.size
+//                && lhs.floorNumber == rhs.floorNumber//idk check
+//    }
+    
+    var valid: Bool
+    var plusCode: String
+    var color: UIColor
+    var size: Double
+    var floorNumber: Int
+    
+    init(valid: Bool, plusCode: String, size: Double, floorNumber: Int){
+        self.valid = valid
+        self.plusCode = plusCode
+        self.size = size
+        self.color = UIColor.clear
+        self.floorNumber = floorNumber
+    }
+    
+}
+
+class Room: Square {
+    var name: String
+    var icon: String //image url or UImage? we shall see
+    
+    init(valid: Bool, plusCode: String, size: Double, name: String, icon: String, floorNumber: Int){
+        self.name = name
+        self.icon = icon
+        super.init(valid: valid, plusCode: plusCode, size: size, floorNumber: floorNumber)
+    }
+}
+
+class HallwayPath: Square {
+    var accessible: Bool
+    
+    init(valid: Bool, plusCode: String, size: Double, name: String, icon: String, accessible: Bool, floorNumber: Int){
+        self.accessible = accessible
+        super.init(valid: valid, plusCode: plusCode, size: size, floorNumber: floorNumber)
+    }
+}
+
+class Floor {
+
+    var active: Bool
+    var number: Int
+    var squareCollection: [Square]
+    //var squareCollection: [String: Square]
+    
+    init(active: Bool, number: Int, allSquares: [Square]){
+        self.active = active
+        self.number = number
+        squareCollection = allSquares
+    }
+    
+//    static func == (lhs: Floor, rhs: Floor) -> Bool {
+//        return
+//            lhs.active = rhs.active
+//            && lhs.number == rhs.number
+//            //idk check
+//    }
+}
+
+class Building {
+    //var floorCollection: [Floor: Int]
+    var floorCollection: [Floor]
+    
+    init(allFloors: [Floor]){
+        floorCollection = allFloors // for now
+    }
+}
--- app/umaps/ViewController.swift
+++ app/umaps/ViewController.swift
@@ -7,10 +7,10 @@
 //
 
 import GoogleMaps
+import GooglePlaces
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,46 @@ class ViewController: UIViewController {
         let camera = GMSCameraPosition.camera(withLatitude: 37.428188, longitude: -122.174188, zoom: 48.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
+        
+        
+        var locationManager : CLLocationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        
+        mapView.isMyLocationEnabled = true;
+        
+        var testPath = GMSMutablePath();
+        testPath.add(CLLocationCoordinate2D(latitude: 37.427761,longitude: -122.174803)) //bl
+        testPath.add(CLLocationCoordinate2D(latitude: 37.428030,longitude: -122.174704)) //tl
+        testPath.add(CLLocationCoordinate2D(latitude: 37.427965,longitude: -122.174327)) //tr
+        testPath.add(CLLocationCoordinate2D(latitude: 37.428043,longitude: -122.174300)) //tr
+        testPath.add(CLLocationCoordinate2D(latitude: 37.428127,longitude: -122.174372)) //o
+        testPath.add(CLLocationCoordinate2D(latitude: 37.428237,longitude: -122.174337)) //o
+        testPath.add(CLLocationCoordinate2D(latitude: 37.428285,longitude: -122.174232)) //o
+        
+        
+        testPath.add(CLLocationCoordinate2D(latitude: 37.428257,longitude: -122.174080)) //o
+        testPath.add(CLLocationCoordinate2D(latitude: 37.428161,longitude: -122.174018)) //o
+        testPath.add(CLLocationCoordinate2D(latitude: 37.428055,longitude: -122.174047)) //o
+        testPath.add(CLLocationCoordinate2D(latitude: 37.428010,longitude: -122.174160)) //o
+        
+        testPath.add(CLLocationCoordinate2D(latitude: 37.427971,longitude: -122.174176)) //r
+        testPath.add(CLLocationCoordinate2D(latitude: 37.427855,longitude: -122.173722)) //r
+        testPath.add(CLLocationCoordinate2D(latitude: 37.427805,longitude: -122.173797)) //r
+        testPath.add(CLLocationCoordinate2D(latitude: 37.427827,longitude: -122.173908)) //r
+        testPath.add(CLLocationCoordinate2D(latitude: 37.427603,longitude: -122.173985)) //br
+        
+        
+        
+        var testPolygon = GMSPolygon(path: testPath)
+        
+        testPolygon.fillColor = UIColor(red : 0, green: 0, blue: 100, alpha: 0.05);
+        testPolygon.strokeColor = .black
+        testPolygon.strokeWidth = 2
+        testPolygon.map = mapView
+        testPolygon.isTappable = true
+        
+        
     }
-
 
 }
